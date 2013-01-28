106c01866d75d418818095c2bac1729247224294
Stephen Rosen <sirosen@uchicago.edu>
2013-01-28 14:15:38 -0600

    Finished basic HMM initialization, fixed up some minor issues.

--- model.py
+++ model.py
@@ -5,9 +5,11 @@
 from __future__ import division
 
 #Takes in a value and a mapping from keys onto probabilities, and returns the key selected by that value
-def select_from_probability(value, probability_dict):
+def select_from_probability_dict(value, probability_dict):
     running_total = 0
-    for k in probability_dict:
+    #sort the dictionary by keys, so that we are guaranteed consistent behavior
+    #even when values are added & removed from the dict
+    for k in sorted(probability_dict):
         v = probability_dict[k]
         running_total += v
         if running_total >= value: return k
@@ -30,7 +32,7 @@ class HMM(object):
         KWArgs:
             pi_values
             A dictionary mapping states (integers from 0 to the number of states) to starting probabilities.
-            Any unassigned starting probabilities will be taken to be equal portions of the remaining probability mass.
+            Any unassigned probabilities will be taken to be equal portions of the remaining probability mass.
 
             transition_map
             A map from ordered index pairs onto transition probabilities.
@@ -39,9 +41,12 @@ class HMM(object):
             emission_map
             A map from states to emission probabilities for each symbol.
             Emission probabilities are themselves represented by a map from symbols to probabilities.
+            Any unassigned probabilities will be taken to be equal portions of the remaining probability mass.
         """
         self.states = frozenset(range(numstates))
         self.alphabet = frozenset(alphabet)
+
+        assert (len(self.alphabet) != 0)
 
         #Initialize the pi values
         #start the probability mass at 1, and reduce it for every element of pi_values
@@ -78,9 +83,10 @@ class HMM(object):
                         self.transition_map[(s1,s2)] = p
 
         #Initialize the emission map
-        #assign equal probability to each letter in each state
         self.emission_map = {}
         for s in self.states:
+            #If the state has nothing specified, it takes on the reasonable default
+            #assign equal probability to each letter in each state
             if s not in emission_map:
                 p = 1/len(self.alphabet)
                 self.emission_map[x] = { l:p for l in self.alphabet }
@@ -89,10 +95,14 @@ class HMM(object):
                 numvalues = len(self.alphabet)
                 state_map = emission_map[s]
                 self.emission_map[s] = {}
+                #Write all of the values that we have into the map
                 for l in state_map:
                     v = state_map[l]
                     self.emission_map[s][l] = v
                     mass -= v
                     numvalues -= 1
-                for l in self.alphabet.difference(state_map.keys()):
-                    self.emission_map[s][l] = 
+                #Assign the remainder probability
+                if numvalues > 0:
+                    p = mass / numvalues
+                    for l in self.alphabet.difference(state_map.keys()):
+                        self.emission_map[s][l] = p
